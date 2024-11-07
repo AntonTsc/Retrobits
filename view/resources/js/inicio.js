41577f1c0cbb420db782def92b9bf6b88ca7e2a6
AnderPati <74302682+AnderPati@users.noreply.github.com>
2024-11-07 22:41:35 +0100

    Swiper por seccion empezado

--- view/resources/js/inicio.js
+++ view/resources/js/inicio.js
@@ -1,34 +1,71 @@
 //Slider
-const swiper = new Swiper('.slider-wrapper', {
-    loop: false,
-    grabCursor: true,
+const swiperDescuentos = new Swiper('.slider-descuentos', {
     spaceBetween: 10,
     centeredSlides: false,
-    slideShadows: true,
-  
+    
+    autoplay: {
+      delay: 5000,
+      pauseOnMouseEnter: true
+    },
+    
     // Pagination bullets
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
       dynamicBullets: true
     },
-  
+    
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
+    
     // Responsive breakpoints
     breakpoints: {
       0: {
-        slidesPerView: 2.5
+        slidesPerView: 1,
+        spaceBetween: 20
       },
-      768: {
-        slidesPerView: 3.5
+      576: {
+        slidesPerView: 2.5,
+        spaceBetween: 10
       },
       1024: {
-        slidesPerView: 6.5
+        slidesPerView: 5.5
+      }
+    }
+  });
+
+  const swiperConsolas = new Swiper('.slider-seccion', {
+    spaceBetween: 10,
+    centeredSlides: false,
+    
+    // Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+    
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    // Responsive breakpoints
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      576: {
+        slidesPerView: 2.5,
+        spaceBetween: 10
+      },
+      1024: {
+        slidesPerView: 5.5
       }
     }
   });
@@ -39,11 +76,47 @@ async function obtenerProductos() {
     try {
         const response = await fetch('/Retrobits/controller/productos.php');
         const productos = await response.json();
-        productos.sort(function(a, b){return b.descuento - a.descuento});
         console.log(productos);
 
+        let count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0;
+        for (let i = 0; i < productos.length; i++) {
+          switch (productos[i].idSeccion) {
+            case "1":
+              count1 < 6 && generador(productos[i], "consolas");
+              count1++;
+              console.log("count1:", count1);
+              break;
+            case "2":
+              count2 < 6 && generador(productos[i], "computadoras");
+              count2++;
+              console.log("count2:", count2);
+              break;
+            case "3":
+              count3 < 6 && generador(productos[i], "camaras");
+              count3++;
+              console.log("count3:", count3);
+              break;
+            case "4":
+              count4 < 6 && generador(productos[i], "radios");
+              count4++;
+              console.log("count4:", count4);
+              break;
+            case "5":
+              count5 < 6 && generador(productos[i], "telefonos");
+              count5++;
+              console.log("count5:", count5);
+              break;
+            case "6":
+              count6 < 6 && generador(productos[i], "electrodomesticos");
+              count6++;
+              console.log("count6:", count6);
+              break;
+          }
+        }
+
+        productos.sort(function(a, b){return b.descuento - a.descuento});
         for (let i = 0; i < 12; i++) {
-            generador(productos[i])
+            generador(productos[i], "descuentos")
         }
     } catch (error) {
         console.error('Error:', error);
@@ -56,19 +129,49 @@ window.onload = function () {
 };
 
 //Genera cartas con los descuentos mas altos
-function generador(producto) {
+function generador(producto, seccion) {
     const button = document.createElement("button");
-    button.classList = "card-item swiper-slide";
+    button.classList = "card-item swiper-slide btn rounded-0";
   
     const img = document.createElement("img");
-    img.src = "https://www.chollogames.es/88016-large_default/famicom-mini-classic.jpg"
+    img.src = `view/resources/images/img/${producto.nombre}.jpg`;
     img.alt = "...";
 
-    const p = document.createElement("p");
-    p.classList = "card-title fs-4"
-    p.innerHTML = producto.nombre;
+    const pNombre = document.createElement("p");
+    pNombre.classList = "text-start card-title text-dark fs-5 px-1"
+    pNombre.innerHTML = producto.nombre;
+
+    const pPrecio = document.createElement("p");
+    pPrecio.classList = "text-start card-title text-dark fs-5 px-1"
+
+    if(producto.descuento > 0){
+      const spanRebajado = document.createElement("span");
+      spanRebajado.classList = "fs-5"
+      spanRebajado.innerHTML = `${(producto.precio*(1 - (producto.descuento/100))).toFixed(2)}€ `;
+  
+      const spanNoRebajado = document.createElement("span");
+      spanNoRebajado.classList = "text-decoration-line-through text-dark text-opacity-75 fs-6"
+      spanNoRebajado.innerHTML = `${producto.precio}€`;
+  
+      pPrecio.appendChild(spanRebajado);
+      pPrecio.appendChild(spanNoRebajado);
+
+      const span = document.createElement("span");
+      span.classList = "position-absolute top-0 end-0 badge rounded-0 bg-danger"
+      span.innerHTML = `-${producto.descuento}%`;
+
+      button.appendChild(span);
+    }else{
+      const spanPrecio = document.createElement("span");
+      spanPrecio.classList = "fs-5"
+      spanPrecio.innerHTML = `${producto.precio}€ `;
+      pPrecio.appendChild(spanPrecio);
+    }
+
   
     button.appendChild(img);
-    button.appendChild(p);
-    document.getElementById("card-list").appendChild(button);
+    button.appendChild(pNombre);
+    button.appendChild(pPrecio);
+    let cardList = document.getElementById(`card-list-${seccion}`);
+    cardList.insertBefore(button, cardList.firstChild);
   }
