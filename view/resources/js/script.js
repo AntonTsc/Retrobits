c1fa4bc224fb1e9602c125b2da291e4df4f7be32
Pablo Perucha <p_peruchacab@fpzornotza.com>
2024-11-12 12:49:45 +0100

    Ampliada funcionalidad ELIMINAR con el array proporcionado. Probado solo con anonymous. Falta que revise el usuario que compra.

--- view/resources/js/script.js
+++ view/resources/js/script.js
@@ -1,13 +1,23 @@
 //GENERACIÓN DE UN JSON PROVISIONAL PARA LA LECTURA DE DATOS DEL CARRITO DIRECTAMENTE DEL MISMO
- let arrayProductos = [
-             {idProducto:'1', cantidad:'11'},
-             {idProducto:'2', cantidad:'111'},
-             {idProducto:'3', cantidad:'1111'}
-         ];
+let cesta = {
+    anonymous: {
+      0:{
+        cantidad: 1,
+        deleted: "0",
+        descripcion: "Laptop de alto rendimiento para profesionales",
+        descuento: "5",
+        id: "4",
+        idSeccion: "2",
+        nombre: "MacBook Pro",
+        precio: "2399.99",
+        stock: "30"
+      }
+    }
+  };
           
-            localStorage.setItem("prod", JSON.stringify(arrayProductos));
-            let productos = JSON.parse(localStorage.getItem("prod"));
-            console.log(localStorage)
+    localStorage.setItem("prod", JSON.stringify(cesta));
+    let productos = JSON.parse(localStorage.getItem("prod")).anonymous;
+    console.log(localStorage)
 
 async function verProductos(){
     try{
@@ -20,27 +30,37 @@ async function verProductos(){
 
         tbody.innerHTML = '';
 
-        productos.forEach(producto => {
+        Object.keys(productos).forEach(eleccion => {
+
+            const producto = productos[eleccion];
             const fila = document.createElement('tr');
 
             //Id del producto añadido al carrito
             const celdaIdProducto = document.createElement('td');
-            celdaIdProducto.textContent = producto.idProducto;
+            celdaIdProducto.textContent = producto.id;
             fila.appendChild(celdaIdProducto);
-            //Aquí debe mostrar nombre ------------------------->PENDIENTE
+            
+            //Muestra el nombre del producto añadido al carrito
+            const celdaNombre = document.createElement('td');
+            celdaNombre.textContent = producto.nombre;
+            fila.appendChild(celdaNombre);
 
             //Cantidad de dicho producto
             const celdaCantidad = document.createElement('td');
             celdaCantidad.textContent = producto.cantidad;
             fila.appendChild(celdaCantidad);
+            
+            //Precio de dicho producto
+            const celdaPrecio = document.createElement('td');
+            celdaPrecio.textContent = producto.precio;
+            fila.appendChild(celdaPrecio);
 
             //Botón para eliminar producto del carrito
             const celdaBotonEliminar = document.createElement('td');
                     const botonEliminar = document.createElement('button');
                     botonEliminar.textContent = 'Eliminar producto del carrito';
                     botonEliminar.onclick = function () {
-                        // arrayProductos.removeItem(producto.idProducto);
-                        eliminarProductoCarrito(producto.idProducto);
+                        eliminarProductoCarrito(eleccion);
                     };
                     celdaBotonEliminar.appendChild(botonEliminar);
                     fila.appendChild(celdaBotonEliminar);
@@ -53,10 +73,10 @@ async function verProductos(){
     }
 }
 
-function eliminarProductoCarrito(idProducto){
+function eliminarProductoCarrito(eleccion){
 
-    productos = productos.filter(producto => producto.idProducto !== idProducto);
-    localStorage.setItem("prod", JSON.stringify(productos));
+    delete productos[eleccion];
+    localStorage.setItem("prod", JSON.stringify({anonymous: productos}));
     verProductos();
 
 }
